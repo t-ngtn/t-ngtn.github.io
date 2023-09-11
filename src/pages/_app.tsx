a63d4dc178446869537d28097c2880d8b12dffbb
t-ngtn <obutaiei@gmail.com>
2023-09-12 00:28:27 +0900

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,15 @@ import "@/styles/globals.css";
 import { AppProps } from "next/app";
 import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps){
-    return (
-        <>
-          <Head>
-            <title>t-ngtn home page</title>
-            <meta
-              name="description"
-              content="t-ngtn's portfolio and blog site"
-            />
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-              <Component {...pageProps} />
-        </>
-      );
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>t-ngtn home page</title>
+        <meta name="description" content="t-ngtn's portfolio and blog site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/posts/001.tsx
+++ src/pages/posts/001.tsx
@@ -1,7 +1,7 @@
 export default function Post001() {
-    return (
-        <div> 
-            <h1>Post001</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Post001</h1>
+    </div>
+  );
 }
