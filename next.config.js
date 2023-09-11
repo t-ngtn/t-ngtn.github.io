b7091da040b9875fddfb046a6d63b7865fab62ec
t-ngtn <obutaiei@gmail.com>
2023-09-11 23:35:58 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // basePath: '/t-ngtn.github.io',
+    // assetPrefix: '/t-ngtn.github.io/',
+}
 
 module.exports = nextConfig
