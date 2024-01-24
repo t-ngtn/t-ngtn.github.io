90326d3d778f067c5d7b6b883f72276ae63b6fe0
t-ngtn <obutaiei@gmail.com>
2024-01-24 13:38:01 +0900

    Export

---
+++ _next/static/chunks/pages/contact-2f048efc2ad20b88.js
@@ -0,0 +1,70 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [335],
+  {
+    1382: function (t, n, e) {
+      (window.__NEXT_P = window.__NEXT_P || []).push([
+        "/contact",
+        function () {
+          return e(1721);
+        },
+      ]);
+    },
+    1721: function (t, n, e) {
+      "use strict";
+      e.r(n),
+        e.d(n, {
+          default: function () {
+            return i;
+          },
+        });
+      var s = e(5893);
+      function i() {
+        return (0, s.jsxs)("div", {
+          className: "mx-auto max-w-screen-lg",
+          children: [
+            (0, s.jsx)("h1", {
+              className: "text-7xl mt-10 font-semibold",
+              children: "Contact",
+            }),
+            (0, s.jsxs)("div", {
+              className: "mt-10",
+              children: [
+                (0, s.jsxs)("div", {
+                  className: "flex text-3xl",
+                  children: [
+                    (0, s.jsx)("p", {
+                      className: "min-w-[80px]",
+                      children: "mail:",
+                    }),
+                    (0, s.jsx)("p", { children: "ngtn.work[at]gmail.com" }),
+                  ],
+                }),
+                (0, s.jsxs)("div", {
+                  className: "flex text-3xl mt-8",
+                  children: [
+                    (0, s.jsx)("p", {
+                      className: "min-w-[80px]",
+                      children: "X:",
+                    }),
+                    (0, s.jsx)("a", {
+                      className:
+                        "text-indigo-500 hover:text-indigo-600 visited:text-indigo-400",
+                      href: "https://twitter.com/ngtn_t",
+                      children: "@ngtn_t",
+                    }),
+                  ],
+                }),
+              ],
+            }),
+          ],
+        });
+      }
+    },
+  },
+  function (t) {
+    t.O(0, [774, 888, 179], function () {
+      return t((t.s = 1382));
+    }),
+      (_N_E = t.O());
+  },
+]);
