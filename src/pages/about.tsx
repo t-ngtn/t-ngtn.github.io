45a96278c4619dd12e2e18170927c76074c94031
t-ngtn <obutaiei@gmail.com>
2024-01-24 13:38:01 +0900

    Export

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,19 +5,29 @@ export default function About() {
 
       <div className="flex">
         <div className="flex-grow">
-          <p className="text-3xl mt-10">Name: Tomoya Nagatani (t-ngtn)</p>
-          <p className="text-3xl mt-10">University: The University of Tokyo</p>
-          <div className="text-3xl mt-10">
-            Major: Computer Science
-            <ul className="list-disc ml-20 text-2xl">
-              <li>Computer Architecture</li>
-              <li>High-Level Synthesis</li>
-              <li>Accelerator Development</li>
-            </ul>
+          <div className="flex text-3xl mt-10">
+            <p className="min-w-[150px]">Name:</p>
+            <p>Tomoya Nagatani (t-ngtn)</p>
           </div>
-          <p className="text-3xl mt-10">
-            Interest: Low-level & Web development
-          </p>
+          <div className="flex text-3xl mt-10">
+            <p className="min-w-[150px]">University:</p>
+            <p>The University of Tokyo</p>
+          </div>
+          <div className="flex text-3xl mt-10">
+            <p className="min-w-[150px]">Major:</p>
+            <div>
+              <p>Computer Science</p>
+              <ul className="list-disc ml-10 text-2xl">
+                <li>Computer Architecture</li>
+                <li>High-Level Synthesis</li>
+                <li>Accelerator Development</li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex text-3xl mt-10">
+            <p className="min-w-[150px]">Interest:</p>
+            <p>Low-level & Web development</p>
+          </div>
         </div>
         <div className="flex-none">
           <img
